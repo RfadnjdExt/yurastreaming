9a3497fd69ca0ab1c6775259f24373234d43f90f
RfadnjdExt <raficanggih@gmail.com>
2023-01-26 22:38:34 +0700

    add status

--- src/routes/api/drive/[...path]/+server.ts
+++ src/routes/api/drive/[...path]/+server.ts
@@ -3,5 +3,5 @@ import type { RequestEvent } from "@sveltejs/kit";
 export async function GET(req: RequestEvent) {
     const { default: axios } = await import('axios')
     const response = await axios.get(`https://hidden-field-da35.yurasubs.workers.dev/${req.params.path}`, { responseType: 'stream' })
-    return new Response(response.data)
+    return new Response(response.data, { status: response.status })
 }
